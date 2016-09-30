0f4403057ec25184565901dc2abb1baf2283da73
k0r0tk0ff <korotkov34reg@gmail.com>
2016-09-30 11:21:09 +0000

    task 9 done

--- src/main/java/ru/parsentev/TEST/TEST.java
+++ src/main/java/ru/parsentev/TEST/TEST.java
@@ -4,6 +4,8 @@ package ru.parsentev.TEST;
 
 import ru.parsentev.task_008.Primes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -11,27 +13,76 @@ import static java.util.Arrays.asList;
 /**
  * Created by root on 9/26/16.
  */
-public class TEST {
+
+/** public class TEST {
+
+    private final String line;
+
+    public TEST(final String line) {
+        this.line = line;
+    }
+
+    public int unique() {
+
+        //int unique_char_count = 0;
+
+        int input_string_lenght = line.length();
+
+        int[] count_of_repeat_char = new int[input_string_lenght];
+
+        char[] char_array_from_string = new char[line.length()];
+
+        char_array_from_string = line.toCharArray();
+
+        ArrayList<Character> buffer = new ArrayList<>();
+        for(char buffered_char: char_array_from_string){
+            buffer.add(buffered_char);
+        }
+
+
+        for(int i=0; i<input_string_lenght; i++){
+            for(int j=1; j<input_string_lenght; j++){
+                if(buffer.get(i) == buffer.get(j)){
+                    count_of_repeat_char[i]++;
+                    input_string_lenght--;
+                    buffer.remove(j);
+                }//else {
+                 //   unique_char_count++;
+                }
+            }
+
+        return input_string_lenght;
+        }
 
     public static void main(String[] arg){
 
-        Primes primes = new Primes(10);
-        List<Integer> result = primes.calc();
+        TEST test = new TEST("aaab");
+        System.out.println(test.unique());
+    }
 
-        String buffer = "";
+}
 
-        for (Integer item: result)
+
+*/
+
+
+
+
+
+/**
+    Primes primes = new Primes(10);
+    List<Integer> result = primes.calc();
+
+    String buffer = "";
+
+for (Integer item: result)
         {
-            buffer = buffer + item + " ";
+        buffer = buffer + item + " ";
         }
 
         System.out.println(buffer);
 
         System.out.println(asList(result));
 
-    }
-}
-
-
-
+ */
 
--- src/main/java/ru/parsentev/task_009/UniqueChars.java
+++ src/main/java/ru/parsentev/task_009/UniqueChars.java
@@ -1,5 +1,7 @@
 package ru.parsentev.task_009;
 
+import java.util.ArrayList;
+
 /**
  * TODO: comment
  *
@@ -14,6 +16,38 @@ public class UniqueChars {
     }
 
     public int unique() {
-        throw new UnsupportedOperationException();
+
+        int input_string_lenght = line.length();
+
+        /**
+         * array`s element counting from 0, bun count of unique is counting from 1
+         *
+         * +1 need for correct counting
+         */
+        int unique_char_count = line.length() + 1;
+
+        /**
+         * Separate string to char array
+         */
+        char[] char_array_from_string = line.toCharArray();
+
+        /**
+         * Convert to ArrayList for work with array's index
+         */
+        ArrayList<Character> buffer = new ArrayList<>();
+        for(char buffered_char: char_array_from_string){
+            buffer.add(buffered_char);
+        }
+
+        for(int i=0; i<input_string_lenght; i++){
+            for(int j=1; j<input_string_lenght; j++){
+                if(buffer.get(i).equals(buffer.get(j))){
+                            buffer.remove(j);
+                            unique_char_count--;
+                            input_string_lenght--;
+                }
+            }
+        }
+        return unique_char_count;
     }
 }
